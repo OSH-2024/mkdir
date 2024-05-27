849cf795b30131eb110823f5643e5a7da998642e
Wxh <wangxianghui@mail.ustc.edu.cn>
2024-05-27 04:52:51 +0000

    Rewrite the  part from lines 407 to 420

---
+++ Main_Rewrite_Work_Summary/Wang_Xianghui/code/__set_printk_clr_event.rs
@@ -0,0 +1,22 @@
+use std::io::{self, Write};
+
+fn set_printk_clr_event() -> io::Result<()> {
+    /*
+     * This program might be calling bpf_trace_printk,
+     * so enable the associated bpf_trace/bpf_trace_printk event.
+     * Repeat this each time as it is possible a user has
+     * disabled bpf_trace_printk events.  By loading a program
+     * calling bpf_trace_printk() however the user has expressed
+     * the intent to see such events.
+     */
+    if trace_set_clr_event("bpf_trace", "bpf_trace_printk", 1)? {
+        writeln!(io::stderr(), "could not enable bpf_trace_printk events");
+    }
+    Ok(())
+}
+
+// 将来需要根据实际情况定义这个函数
+fn trace_set_clr_event(event1: &str, event2: &str, flag: i32) -> io::Result<bool> {
+    // 实现这个函数
+    Ok(false)
+}
