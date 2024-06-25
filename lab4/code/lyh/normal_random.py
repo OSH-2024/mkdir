92a05ff3260a3a88e4961b56449630e6347deb1f
XHuiWang <wangxianghui@mail.ustc.edu.cn>
2024-06-25 20:23:27 +0800

    Remove some commented-out code

--- lab4/code/lyh/normal_random.py
+++ lab4/code/lyh/normal_random.py
@@ -1,5 +1,4 @@
 import random
-#import math
 import time
 const_n=1_0000 #随机游走次数
 const_step=10_0000 #每次随机游走步数
@@ -12,7 +11,6 @@ def random_walk(n):
     return (x, y)
 
 start_time=time.time()
-#distance=0
 n=const_n
 step=const_step
 x=0
@@ -21,5 +19,4 @@ for i in range(n):
     walk = random_walk(step)
     x+=walk[0]
     y+=walk[1]
-    #distance=distance+math.sqrt(walk[0]**2+walk[1]**2)
 print(x/n,y/n,time.time()-start_time)
